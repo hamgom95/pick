9b3cd6b940c048ac673afef345d89ce581e94495
Kevin Thomas <hamgom95@gmail.com>
2019-02-10 17:21:29 +0100

    add formating

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
  * Typed that:
  * - keys to extract have to exist in obj
  * - returned obj has right types for extracted obj
- * 
+ *
  * @param obj   Object to extract from
  * @param props Properties to extract.
  * @returns     Object with extracted properties.
  */
 const pick = (obj, ...props) =>
-    props.reduce((state, prop) => (state[prop]=obj[prop], state), {});
+    props.reduce((state, prop) => ((state[prop] = obj[prop]), state), {});
 
 module.exports = {
     pick
--- index.spec.js
+++ index.spec.js
@@ -1,18 +1,22 @@
-const {expect} = require("chai");
-const {pick} = require("./index");
+const { expect } = require("chai");
+const { pick } = require("./index");
 
-describe('pick()', function () {
-    
+describe("pick()", function() {
     const obj = {
         a: 1,
         b: 2,
-        c: 3,
+        c: 3
     };
-    
-    it('should contain extracted values', function () {
 
-        expect(pick(obj, "a", "b"), "Wrong keys extracted").to.have.all.keys("a", "b");
-        expect(Object.values(pick(obj, "a", "b")), "Wrong (ordered) values for keys").to.have.ordered.members([1, 2]);
+    it("should contain extracted values", function() {
+        expect(pick(obj, "a", "b"), "Wrong keys extracted").to.have.all.keys(
+            "a",
+            "b"
+        );
+        expect(
+            Object.values(pick(obj, "a", "b")),
+            "Wrong (ordered) values for keys"
+        ).to.have.ordered.members([1, 2]);
         expect(pick(obj, "a")).to.have.property("a", 1);
     });
 });
